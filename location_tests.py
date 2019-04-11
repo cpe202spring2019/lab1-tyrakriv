f82238cace99430c5dc79ce2a6b29afef2f5996a
tyrakriv <tytykr@gmail.com>
2019-04-10 20:31:05 -0700

    final final

--- location_tests.py
+++ location_tests.py
@@ -15,6 +15,10 @@ class TestLab1(unittest.TestCase):
         """Tests the location of Seattle to find if it represented correctly"""
         loc3 = Location("Seattle", 70.8, 117.3)
         self.assertEqual(repr(loc3),"Location(Seattle,70.8,117.3)")
+
+        """Checks with integers"""
+        loc = Location('Loveland', 20, 19)
+        self.assertEqual(repr(loc), "Location(Loveland,20,19)")
         
     def test_eq(self):
         """Tests the eq function to determine if a location matches its description"""
@@ -31,9 +35,13 @@ class TestLab1(unittest.TestCase):
 
         """Tests loc3 against loc4"""
         self.assertTrue(loc3 == loc4)
-
+        self.assertEqual(loc3, loc4)
+        
         """Tests if loc1 against loc5"""
         self.assertTrue(loc1 == loc5)
+
+        """Tests if wrong type"""
+        self.assertFalse(loc1 == "SLO")
         
     def test_init(self):
         """Checks init function to determine if the objects in the class are correct"""
@@ -47,6 +55,14 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(loc2.name, 'Tokyo')
         self.assertEqual(loc2.lat, 178.2)
         self.assertEqual(loc2.lon, -5.4)
+
+        loc1 = Location("SLO",  35.3, -120.7)
+        other = Location("SLO", 35.3, -120.7)
+        """Test the equal function for different locations with same values"""
+        self.assertEqual(loc1, other)
+        other = loc1
+        """Test the equal function for same location"""
+        self.assertEqual(loc1, other)
 
 if __name__ == "__main__":
         unittest.main()
